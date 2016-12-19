a5f8a8a52472e501e0e2d8dd7a5280605268d968
Nicolas Quartier <quartier.nicolas@gmail.com>
2016-12-19 23:06:33 +0100

    Changed image folder (test if work on opnshift too? )

--- src/main/java/kristdepuydt/controllers/HomeController.java
+++ src/main/java/kristdepuydt/controllers/HomeController.java
@@ -30,7 +30,8 @@ public class HomeController {
     @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
     public String index(Model model) {
 
-        if(System.getProperty("runLocal").equals("true")) {
+        //-DrunLocal=true
+        if(System.getProperty("runLocal") != null && System.getProperty("runLocal").equals("true")) {
             Path currentRelativePath = Paths.get("");
             String currentRelativePathAsString = currentRelativePath.toAbsolutePath().toString() + "/../";
             if(!new File(mainFolder).isAbsolute()) {
