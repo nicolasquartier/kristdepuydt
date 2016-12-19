7032728f76d33310441607ae3387ca6b478093cc
Nicolas Quartier <quartier.nicolas@gmail.com>
2016-12-19 22:22:49 +0100

    Changed image folder (test if work on opnshift too? )

--- src/main/java/kristdepuydt/controllers/HomeController.java
+++ src/main/java/kristdepuydt/controllers/HomeController.java
@@ -1,10 +1,10 @@
 package kristdepuydt.controllers;
 
+import kristdepuydt.domain.ImageHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import kristdepuydt.domain.ImageHolder;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -18,8 +18,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RequestMapping("/")
 public class HomeController {
 
-    private static final String mainFolder = "./src/main/webapp/static/img/beelden";
-    private static final String mainFolderShort = "../../static/img/beelden";
+//    private static final String mainFolder = "./src/main/webapp/static/img/beelden";
+    private static final String mainFolder = "img\\beelden";
+    private static final String mainFolderShort = "img\\beelden";
 
     @RequestMapping(value = "/home", method = GET)
     public String home(Model model) {
@@ -30,20 +31,20 @@ public class HomeController {
     public String index(Model model) {
 
         Path currentRelativePath = Paths.get("");
-        String s = currentRelativePath.toAbsolutePath().toString();
+        String currentRelativePathAsString = currentRelativePath.toAbsolutePath().toString() + "\\..\\";
 
         List<ImageHolder> thumbnails = new ArrayList<>();
-        File folderBeelden = new File(mainFolder);
+        File folderBeelden = new File(currentRelativePathAsString + mainFolder);
         if (folderBeelden.list() != null) {
             for (String urlFolderSpecificBeeld : folderBeelden.list()) {
-                File folderSpecificBeeld = new File(mainFolder + "/" + urlFolderSpecificBeeld);
+                File folderSpecificBeeld = new File(currentRelativePathAsString + mainFolder + "\\" + urlFolderSpecificBeeld);
                 if (folderSpecificBeeld.isDirectory()) {
                     List<String> extraBeelden = new ArrayList<>();
                     for (int i = 0; i < folderSpecificBeeld.list().length; i++) {
                         if (i == 0) continue;
-                        extraBeelden.add(mainFolderShort + "/" + urlFolderSpecificBeeld + "/" + folderSpecificBeeld.list()[i]);
+                        extraBeelden.add(mainFolderShort + "\\" + urlFolderSpecificBeeld + "\\" + folderSpecificBeeld.list()[i]);
                     }
-                    thumbnails.add(new ImageHolder(mainFolderShort + "/" + urlFolderSpecificBeeld + "/" + folderSpecificBeeld.list()[0], urlFolderSpecificBeeld, extraBeelden));
+                    thumbnails.add(new ImageHolder(mainFolderShort + "\\" + urlFolderSpecificBeeld + "\\" + folderSpecificBeeld.list()[0], urlFolderSpecificBeeld, extraBeelden));
                 }
             }
         }
